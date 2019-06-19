426c4f8a88bf2ed38a5e38af5481ec44d9d949ae
adusumalliakhil <45625407+adusumalliakhil@users.noreply.github.com>
2019-06-19 11:48:40 +0530

    Add files via upload

---
+++ function calling.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 12 18:35:15 2019
+
+@author: akhil
+"""
+
+#import sys
+
+#sys.path.append("C:/Users/akhil/Documents/dash_python/dash_functions")
+
+from summing_function import sum_it
+
+k=sum_it(5,6)
+print(k)
